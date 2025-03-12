6bc6de34fc5a96a2d396d48946e4bafb8df53099
Coder9892 <jackhypergamer@gmail.com>
2025-03-12 08:14:31 +0700

    connect DB

---
+++ src/main/java/DAO/DataProvider.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package DAO;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+/**
+ *
+ * @author Admin
+ */
+public class DataProvider {
+    public static Connection dataConnection() throws Exception{
+        Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+        String url = "jdbc:sqlserver://localhost:1433;Database=QuanLyKhachSan2";
+        String username = "sa";
+        String password = "123456";
+        
+        Connection con = DriverManager.getConnection(url, username, password);
+        return con;
+    }
+}
